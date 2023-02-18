1db951ae9198417b8895c90e617b8a450567ca01
Beard <beardlyness@users.noreply.github.com>
2023-02-18 04:33:45 -0800

    Create pepe.js

---
+++ js/pepe.js
@@ -0,0 +1,38 @@
+// url Async requesting function
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function()
+    {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+        {
+            callback(xmlHttp.responseText);
+        }
+    }
+    xmlHttp.open("GET", theUrl, true);
+    xmlHttp.send(null);
+    return;
+}
+
+// callback for the top 8 GIFs of search
+function tenorCallback_search(responsetext)
+{
+    var response_objects = JSON.parse(responsetext);
+    top_10_gifs = response_objects["results"];
+    document.getElementById("share_gif").src = top_10_gifs[0]["media"][0]["tinygif"]["url"];
+    document.getElementById("share_uri").href = top_10_gifs[0]["media"][0]["tinygif"]["url"];
+    return;
+}
+
+// function to call the trending and category endpoints
+function grab_data()
+{
+    var apikey = "LIVDSRZULELA";
+    var lmt = 8;
+    var search_term = "pepe";
+    var search_url = "https://g.tenor.com/v1/random?q=" + search_term + "&key=" +
+            apikey + "&limit=" + lmt;
+    httpGetAsync(search_url,tenorCallback_search);
+    return;
+}
+grab_data();
